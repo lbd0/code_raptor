32c4ddcd113afc5acb60ff4007003b72fdbc1c57
hyeeunS2 <hyesilver781@gmail.com>
2025-09-08 19:51:25 +0900

    [HE] Update 물부족

--- 물_부족/HE.cpp
+++ 물_부족/HE.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int solution(int storage, int usage, vector<int> change) {
     int total_usage = 0;
     for(int i=0; i<change.size(); i++){
-        usage = usage + (usage * change[i] / 100); //지난 달 물 계산
+        usage = usage + (usage * change[i] / 100); //지난 달 물 사용량 계산
         total_usage += usage;
         if(total_usage > storage){
             return i;
