15aeaa3a9033ef90a8c7d900699c799e89ea6c3e
hyeeunS2 <hyesilver781@gmail.com>
2025-09-08 19:53:07 +0900

    [HE] Update 최소직사각형

--- 최소직사각형/HE.cpp
+++ 최소직사각형/HE.cpp
@@ -10,7 +10,7 @@ int solution(vector<vector<int>> sizes) {
     
     for(int i = 0; i < sizes.size(); i++){
         if(sizes[i][0] < sizes[i][1]) //명함 크기 비교
-            swap(sizes[i][1],sizes[i][0]); //명함 회전
+            swap(sizes[i][1], sizes[i][0]); //명함 회전
         
         w = max(w, sizes[i][0]);
         h = max(h, sizes[i][1]); //명함 가로 세로 길이 최댓값 찾아서 계산
