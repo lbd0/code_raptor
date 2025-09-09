d52081f71ff0923c4e38de014daefc4d6709f818
cogusp <84373005+cogusp@users.noreply.github.com>
2025-09-09 21:34:30 +0900

    [CH] upload
    upload

--- 2. 시저_암호/CH.cpp
+++ 2. 시저_암호/CH.cpp
@@ -3,12 +3,12 @@
 std::string solution(std::string s, int n) {
     for (char &c : s) {
         // 공백은 그대로 추가
-        if (c == ' ') continue; 
+        if (c == ' ') continue;
         // 소문자일 경우
         else if (c >= 'a' && c <= 'z') c = (c - 'a' + n) % 26 + 'a';
         // 대문자일 경우
         else if (c >= 'A' && c <= 'Z') c = (c - 'A' + n) % 26 + 'A';
     }
-
+    
     return s;
 }
