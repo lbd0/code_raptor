4d6de14c99217194b79f4b4dfbb9642aba192328
hyeeunS2 <hyesilver781@gmail.com>
2025-09-11 19:14:25 +0900

    [HE] Update 더맵게

--- 4.더_맵게/HE.cpp
+++ 4.더_맵게/HE.cpp
@@ -1,1 +1,25 @@
-//더 맵게
+//힙 설명 다시 읽으면서 풀었어요
+#include <vector>
+#include <queue>
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end()); //top()값 최소로
+    int answer = 0;
+
+    while(pq.top() < K) {
+        if(pq.size() < 2) //스코빌 지수를 K 이상으로 만들 수 없는 경우 = 음식 수가 부족한 경
+            return -1;
+
+        int first = pq.top(); // pq.top() = 가장 지수 낮은 음식
+        pq.pop();
+        int second = pq.top();
+        pq.pop();
+
+        int food = first + (second * 2);
+        pq.push(food);
+        answer++;
+    }
+
+    return answer;
+}
