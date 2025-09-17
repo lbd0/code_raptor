75ba2205ee6cd867e9bcd2f4b268a1e10bb19c5a
hyeeunS2 <hyesilver781@gmail.com>
2025-09-17 17:58:04 +0900

    [HE] 인뽑

--- 8.크레인_인형뽑기_게임/HE.cpp
+++ 8.크레인_인형뽑기_게임/HE.cpp
@@ -1,1 +1,30 @@
-//sfs
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+    vector<int> choice; //뽑은 인형들
+
+    for (int i = 0; i < moves.size(); i++) {
+        int move = moves[i] - 1;
+
+        for (int row = 0; row < board.size(); row++) {
+            if (board[row][move] != 0) { // 인형이 있으면 뽑은 인형 저장
+                int doll = board[row][move];
+                board[row][move] = 0; // 뽑은 곳 비우기
+
+                choice.push_back(doll);
+
+                if (choice.size() >= 2 && choice[choice.size() - 2] == choice.back()) { // 이전 인형과 같으면 폭발
+                    choice.pop_back(); // 현재 인형 제거
+                    choice.pop_back(); // 이전 인형 제거
+                    answer += 2;
+                }
+                break;
+            }
+        }
+    }
+
+    return answer;
+}
