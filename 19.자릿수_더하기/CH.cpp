caa6ce19299df97e064daccf37cdf0fc32ffc981
cogusp <84373005+cogusp@users.noreply.github.com>
2025-11-10 21:41:49 +0900

    [CH] upload

--- 19.자릿수_더하기/CH.cpp
+++ 19.자릿수_더하기/CH.cpp
@@ -1,16 +1,13 @@
-#include <iostream>
 using namespace std;
 int solution(int n)
 {
     int answer = 0;
 
-    while (n >= 10)
+    while (n > 0)
     {
         answer += (n % 10);
         n /= 10;
     }
-
-    answer += n;
     
     return answer;
 }
