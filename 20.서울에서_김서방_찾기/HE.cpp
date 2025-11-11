cce9cc96834a9a2778aba99ac2b42e62ed458568
hyeeunS2 <hyesilver781@gmail.com>
2025-11-11 21:15:24 +0900

    [HE]김서방

---
+++ 20.서울에서_김서방_찾기/HE.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "";
+    int x;
+    for(int i = 0; i < seoul.size(); i++){
+        if(seoul[i] == "Kim"){
+            x = i;
+        }
+    }
+    answer = "김서방은 "+ to_string(x) +"에 있다";
+    return answer;
+}
