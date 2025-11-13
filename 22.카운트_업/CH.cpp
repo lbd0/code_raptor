4d2b076d56ad5a2eef772c2ff8a19915c99c503a
cogusp <84373005+cogusp@users.noreply.github.com>
2025-11-13 14:15:36 +0900

    [CH] upload

---
+++ 22.카운트_업/CH.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+
+std::vector<int> solution(int start_num, int end_num) {
+    std::vector<int> answer;
+    
+    for (int i = start_num; i <= end_num; ++i) answer.push_back(i);
+    
+    return answer;
+}
