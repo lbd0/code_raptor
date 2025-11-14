abfe8fa5d2e1f1a3a0b25ac4cda952e96b3add42
cogusp <84373005+cogusp@users.noreply.github.com>
2025-11-14 16:18:09 +0900

    [CH] upload

---
+++ 23.콜라_문제/CH.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    
+    while (n >= a)
+    {
+        answer += b * (n / a);
+        n = (n % a) + b * (n / a);
+    }
+    
+    return answer;
+}
