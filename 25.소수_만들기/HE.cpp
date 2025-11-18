3b69a8d926f12e904b1ab2a4712931ebb9196a82
hyeeunS2 <hyesilver781@gmail.com>
2025-11-18 12:58:56 +0900

    [HE] 소수만들기

---
+++ 25.소수_만들기/HE.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int s) { // 소수 판별 함수
+    if (s < 2) return false; 
+    for (int i = 2; i * i <= s; i++) {
+        if (s % i == 0) return false;
+    }
+    return true; 
+}
+
+int solution(vector<int> nums) {
+    int answer = 0;
+    
+    for(int i = 0; i < nums.size(); i++){
+        for(int j = i + 1; j < nums.size(); j++){
+            for(int k = j + 1; k < nums.size(); k++){     
+                int sum = nums[i] + nums[j] + nums[k];                
+                if (isPrime(sum)) answer++;
+            }
+        }
+    }
+
+    cout << answer << endl;
+
+    return answer;
+}
