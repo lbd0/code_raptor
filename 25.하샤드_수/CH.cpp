a969007f216b11a82ab84f73a2ccd5a80d74d066
cogusp <84373005+cogusp@users.noreply.github.com>
2025-11-18 22:05:15 +0900

    [CH] upload

---
+++ 25.하샤드_수/CH.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+
+bool solution(int x) {
+    int temp = x, sum = 0;
+    
+    while (temp > 0) {
+        sum += temp % 10;
+        temp /= 10;
+    }
+    
+    return x % sum == 0 ? true : false;
+}
