a2078d94b2d994c6396d6ef07f5bf335c6b2560d
BADA LEE <80818640+lbd0@users.noreply.github.com>
2025-12-10 22:11:47 +0900

    [BD] add 푸드 파이트 대회

---
+++ 30.푸드_파이트_대회/BD.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<int> food) {
+    string answer = "";
+    
+    string temp = "";
+    for(int i=1; i<food.size(); ++i)
+    {
+        for(int j=0; j<food[i]/2; ++j)
+        {
+            temp += i + '0';
+        }
+    }
+    
+    answer += temp + "0";
+    
+    reverse(temp.begin(), temp.end());
+    
+    answer += temp;
+    
+    return answer;
+}
